69e72b3f7cee5ac97749dc8619630c7ca66b6766
hijx <queen.spn.spn@gmail.com>
2022-03-25 14:36:12 +0300

    done

--- Random/script.js
+++ Random/script.js
@@ -1,3 +1,21 @@
 // project أنشئ كائن
+const project = {
+    type: "tech",
+    users: 100000,
+    biograghy(){
+    return "هذا مشروعي";
 
+    },
+
+};
 // randomProperty أنشئ دالة
+function randomProperty(x){
+const keysObject = Object.keys(x);
+const randomNumber = Math.floor(Math.random() * keysObject.length);
+console.log(keysObject['${randomNumber}']);
+
+console.log(x['${keysOpject['${randomNumber}']}']);
+
+randomProperty(project);
+}
+
